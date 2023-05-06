193a316332e55956ea09dbc7878b78b380506329
EugenyBobylev <ebobylev@gmail.com>
2023-05-06 04:56:08 +0300

    add orm_tests.py

---
+++ tests/orm_tests.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine, Connection
+from config import BotConfig
+
+
+def test_connect_to_postgres():
+    config = BotConfig.instance()
+    url = config.get_postgres_url()
+    engine = create_engine(url, pool_size=50, echo=False)
+    assert engine is not None
+
+    with engine.connect() as conn:
+        assert conn is not None
+        assert not conn.closed
