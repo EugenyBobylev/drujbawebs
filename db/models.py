310d386304cbdfde1cf7f301bd05f05b70a8fb24
EugenyBobylev <ebobylev@gmail.com>
2023-05-09 12:52:25 +0300

    add db model Fundraising

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, Date, BigInteger, Text, ForeignKey
+from sqlalchemy import Integer, String, Date, BigInteger, Text, ForeignKey, Time
 from sqlalchemy.orm import DeclarativeBase, relationship, mapped_column
 
 
@@ -40,11 +40,12 @@ class Account(Base):
     payed_events = mapped_column(Integer, default=0)
     user_id = mapped_column(BigInteger, ForeignKey('users.id'), nullable=False)
     company_id = mapped_column(Integer, ForeignKey('companies.id'))
-    company_member_id = mapped_column(Integer, ForeignKey('company_members.company_id'))
+    company_member_id = mapped_column(Integer, ForeignKey('companies.id'))
 
     owner = relationship('User', foreign_keys=[user_id], back_populates='accounts')
     company = relationship('Company', foreign_keys=[company_id], uselist=False)
-    member = relationship('CompanyMember', foreign_keys=[company_member_id], cascade="all, delete",)
+    member = relationship('Company', foreign_keys=[company_member_id], uselist=False)
+    fundraisings = relationship('Fundraising')
 
     def __repr__(self) -> str:
         return f'id={self.id}; user_id={self.user_id}; company_id={self.company_id}, ' \
@@ -85,41 +86,33 @@ class CompanyMember(Base):
         return f'company_id={self.company_id}; account_id={self.account_id}'
 
 
-# class Fundraising(Base):
-#     """
-#     Сбор средств (на подарок, на ресторан и т.д.)
-#     """
-#     __tablename__: str = 'fundrasing'
-#
-#     id = mapped_column(Integer, primary_key=True)
-#     reason = mapped_column(String, default='')          # основание для сбора (ДР, юбилей, свадьба, 8-е марта)
-#     target = mapped_column(String, nullable=False)      # кому собираем
-#     owner_id = mapped_column(ForeignKey("users.id"), nullable=False)  # лицо ответственное за сбор
-#     owner = relationship('User')                                      # лицо ответственное за сбор
-#     start = mapped_column(Date, nullable=False)         # дата регистрации сбора
-#     end = mapped_column(Date, nullable=False)           # дата окончания сбора
-#     event_date = mapped_column(Date, nullable=False)    # дата события
-#     transfer_info = mapped_column(Text)                 # реквизиты перевода (номер карты или телефон)
-#     gift_info = mapped_column(Text)                     # варианты подарков
-#     congratulation_date = mapped_column(Date)           # дата праздничного мероприятия
-#     congratulation_time = mapped_column(Time)           # время праздничного мероприятия
-#     event_place = mapped_column(Text)                   # место проведения мероприятия
-#     event_dresscode = mapped_column(Text)               # дресс-код
-#     invite_url = mapped_column(String, nullable=False, default='')  # ссылка приглашения для участия в сборе
-#
-#     def __repr__(self) -> str:
-#         return f'id={self.id}; reason="{self.reason}"; target="{self.target}"; ' \
-#                f'campaign_start={self.start.strftime("%d.%m.%Y")}; ' \
-#                f'campaign_end={self.end.strftime("%d.%m.%Y")};' \
-#                f'owner_id={self.owner_id}'
+class Fundraising(Base):
+    """
+    Сбор средств (на подарок, на ресторан и т.д.)
+    """
+    __tablename__: str = 'fundrasing'
 
+    id = mapped_column(Integer, primary_key=True)
+    reason = mapped_column(String, default='')          # основание для сбора (ДР, юбилей, свадьба, 8-е марта)
+    target = mapped_column(String, nullable=False)      # кому собираем
+    account_id = mapped_column(ForeignKey("accounts.id", ondelete='cascade'), nullable=False)
+    owner = relationship('Account', back_populates='fundraisings')  # лицо ответственное за сбор
+    start = mapped_column(Date, nullable=False)         # дата регистрации сбора
+    end = mapped_column(Date, nullable=False)           # дата окончания сбора
+    event_date = mapped_column(Date, nullable=False)    # дата события
+    transfer_info = mapped_column(String, default='')   # реквизиты перевода (номер карты или телефон)
+    gift_info = mapped_column(Text, default='')         # варианты подарков
+    congratulation_date = mapped_column(Date)           # дата праздничного мероприятия
+    congratulation_time = mapped_column(Time)           # время праздничного мероприятия
+    event_place = mapped_column(String, default='')     # место проведения мероприятия
+    event_dresscode = mapped_column(String, default='')  # дресс-код
+    invite_url = mapped_column(String, nullable=False, default='')  # ссылка приглашения для участия в сборе
 
-# class Member(Base):
-#     fundrasing_id = mapped_column(Integer, ForeignKey("fundrasing.id"), primary_key=True)  # код сбора
-#     user_id = mapped_column(BigInteger, ForeignKey("users.id"), primary_key=True)  # телеграм id пользователя
-#     User = relationship('User', back_populates="members")
-#     transfer_sum = mapped_column(Numeric, nullable=False, default=0)  # сумма взноса
-#     transfer_date = mapped_column(Date)  # дата взноса
+    def __repr__(self) -> str:
+        return f'id={self.id}; reason="{self.reason}"; target="{self.target}"; ' \
+               f'campaign_start={self.start.strftime("%d.%m.%Y")}; ' \
+               f'campaign_end={self.end.strftime("%d.%m.%Y")};' \
+               f'account_id={self.account_id}'
 
 
 class Msg(Base):
