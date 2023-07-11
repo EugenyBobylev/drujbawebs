9ae2f3334febb922d650efe3c166a018daa9add1
NoPasaRun <bogdanbelenesku@gmail.com>
2023-07-11 18:50:54 +0300

    дебаг

--- backend/models.py
+++ backend/models.py
@@ -15,8 +15,8 @@ class WebAppUser:
     id: int
     first_name: str
     last_name: str
-    username: str
     language_code: str
+    username: str = ''
 
     def __repr__(self):
         return f'id={self.id}; first_name={self.first_name} last_name={self.last_name}; username={self.username}'
